b31197bada42215411e749e920024a3604dadb81
colbyfayock <fayock@gmail.com>
2021-06-24 08:30:37 -0400

    02 - Specifying the Next.js target as serverless and updating the page title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Colby&apos;s Store
         </h1>
 
         <p className={styles.description}>
